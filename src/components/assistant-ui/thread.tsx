06ed0bcd641ca143735b22c13dfaf15ac51cf3a9
Lennex Zinyando <brizdigital@gmail.com>
2025-03-11 16:06:08 +0200

    Update hardcoded prompts

--- src/components/assistant-ui/thread.tsx
+++ src/components/assistant-ui/thread.tsx
@@ -88,22 +88,22 @@ const ThreadWelcomeSuggestions: FC = () => {
     <div className="mt-3 flex w-full items-stretch justify-center gap-4">
       <ThreadPrimitive.Suggestion
         className="hover:bg-muted/80 flex max-w-sm grow basis-0 flex-col items-center justify-center rounded-lg border p-3 transition-colors ease-in"
-        prompt="What is the weather in Tokyo?"
+        prompt="Top 10 places to visit in the world?"
         method="replace"
         autoSend
       >
         <span className="line-clamp-2 text-ellipsis text-sm font-semibold">
-          What is the weather in Tokyo?
+          Top 10 places to visit in the world?
         </span>
       </ThreadPrimitive.Suggestion>
       <ThreadPrimitive.Suggestion
         className="hover:bg-muted/80 flex max-w-sm grow basis-0 flex-col items-center justify-center rounded-lg border p-3 transition-colors ease-in"
-        prompt="What is assistant-ui?"
+        prompt="Tell me the trending AI news right now."
         method="replace"
         autoSend
       >
         <span className="line-clamp-2 text-ellipsis text-sm font-semibold">
-          What is assistant-ui?
+          Tell me the trending AI news right now?
         </span>
       </ThreadPrimitive.Suggestion>
     </div>
